7aff8b5def044f338374077d3a2ecc15686845cb
Barry Broom <bbroom@ipmail.org.uk>
2017-01-24 11:37:11 +0000

    Add different stickers for each button press

--- stickers.py
+++ stickers.py
@@ -6,26 +6,32 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.button import Button
+from kivy.properties import StringProperty
 
 class Sticker(Image):
+  def __init__(self, **kwargs):
+    super(Sticker, self).__init__(**kwargs)
+    self.height = 300
+    self.width = 300
+
   def on_touch_move(self, touch):
     if 'pos' in touch.profile:
       self.x = touch.x - (self.width / 2)
       self.y = touch.y - (self.height / 2)
 
 class StickerBookBarButton(Button):
+  sticker_source = StringProperty()    
+
   def on_touch_down(self, touch):
-    self.add_widget(Sticker())
+    self.add_widget(Sticker(source = self.sticker_source))
 
 class StickerBookBar(BoxLayout):
   def __init__(self, **kwargs):
     super(StickerBookBar, self).__init__(**kwargs)
     self.orientation = 'vertical'
-    self.add_widget(StickerBookBarButton(text='A'))
-    self.add_widget(StickerBookBarButton(text='B'))
-    self.add_widget(StickerBookBarButton(text='C'))
-    self.add_widget(StickerBookBarButton(text='D'))
-    self.add_widget(StickerBookBarButton(text='E'))
+    self.add_widget(StickerBookBarButton(id='btn01', text='A', sticker_source='monster01.png'))
+    self.add_widget(StickerBookBarButton(id='btn02', text='B', sticker_source='monster02.png'))
+    self.add_widget(StickerBookBarButton(id='btn03', text='C', sticker_source='monster03.png'))
 
 
 class GameMenu(Widget):
